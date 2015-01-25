e42026b4c7bcbc0515832438857a5aefe75e8549
David Ginzberg <DavidGinzberg@users.noreply.github.com>
2015-01-25 09:58:47 +0200

    Added beginning cookie manipulation

--- js/core.js
+++ js/core.js
@@ -1,1 +1,12 @@
-alert("Hello");
+function alertUserCookie(){
+  document.cookie="user=someone";
+  alert(document.cookie);
+}
+
+function doTheThing(){
+  alertUserCookie();
+  
+  console.log("I did the thing!");
+}
+
+$(document).ready(doTheThing);
